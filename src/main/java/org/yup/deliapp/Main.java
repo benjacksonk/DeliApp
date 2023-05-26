593ed19b5fd7097f1d6d257e727af673b6e3d8c4
Isaac Francisco <isaacfrancisco305@gmail.com>
2023-05-26 13:39:48 -0400

    testing mine

--- src/main/java/org/yup/deliapp/Main.java
+++ src/main/java/org/yup/deliapp/Main.java
@@ -3,5 +3,6 @@ package org.yup.deliapp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("isaacs branch test ");
     }
 }
