8dbddefa6e209a0afbc25e99735fed4f71becfae
Benjamin Jackson <benjacksonk@gmail.com>
2023-05-30 10:50:52 -0400

    added enums for BreadType, MeatType, CheeseType, and FreeTopping

---
+++ src/main/java/org/yup/deliapp/FreeTopping.java
@@ -0,0 +1,21 @@
+package org.yup.deliapp;
+
+public enum FreeTopping {
+    LETTUCE,
+    PEPPERS,
+    ONIONS,
+    TOMATOES,
+    JALAPENOS,
+    CUCUMBERS,
+    PICKLES,
+    GUACAMOLE,
+    MUSHROOMS,
+    MAYO,
+    MUSTARD,
+    KETCHUP,
+    RANCH,
+    THOUSAND_ISLAND,
+    VINAIGRETTE,
+    AU_JUS,
+    SAUCE
+}
