0eb7b974c63defb42d3ee7eb4744a2e813c8d7b6
Benjamin Jackson <benjacksonk@gmail.com>
2023-05-30 13:57:04 -0400

    added OrderItem class, made Drinks extend it

--- src/main/java/org/yup/deliapp/Drinks.java
+++ src/main/java/org/yup/deliapp/Drinks.java
@@ -1,6 +1,6 @@
 package org.yup.deliapp;
 
-public class Drinks {
+public class Drinks extends OrderItem {
     private String size;
     private DrinkFlavor flavor;
 
@@ -9,18 +9,25 @@ public class Drinks {
         this.flavor = flavor;
     }
 
-    public String getPrice(String drinkSize, DrinkFlavor flavor){
-      double price = 0;
-        if(drinkSize.equals("Small")){
-          price = 2.00;
-        } else if (drinkSize.equals("Medium")){
-            price = 2.50;
-        } else if (drinkSize.equals("Large")){
-            price = 3.00;
-        } else {
-          return  "Please choose appropriate size";
-        }
-        return "Here is the cost of your drink: " +  drinkSize + " " + flavor + "  " + price + "$";
+    public double getPrice(){
+      double price;
+
+      switch (size.toUpperCase()) {
+          case "SMALL":
+              price = 2.00;
+              break;
+          case "MEDIUM":
+              price = 2.50;
+              break;
+          case "LARGE":
+              price = 3.00;
+              break;
+          default:
+              price = 0;
+              break;
+      }
+
+      return price;
     }
 
 
