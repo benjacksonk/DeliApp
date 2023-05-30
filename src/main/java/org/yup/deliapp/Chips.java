23126c2830619e75c16de02853d935bd4cf20632
Isaac Francisco <isaacfrancisco305@gmail.com>
2023-05-30 15:18:30 -0400

    my new code for chips

--- src/main/java/org/yup/deliapp/Chips.java
+++ src/main/java/org/yup/deliapp/Chips.java
@@ -1,2 +1,21 @@
-package org.yup.deliapp;public class Chips {
+package org.yup.deliapp;
+
+public class Chips {
+    private final double price = 1.50;
+
+    public enum chipOptions {
+        ORIGINAL_LAYS,
+        FRITOS,
+        BBQ_LAYS,
+        DORITOS,
+        BLUE_DORITOS,
+        ORIGINAL_SUNCHIPS,
+        CHEETOS,
+        TOSTITOS
+    }
+
+    public double getPrice(chipOptions chip) {
+        return price;
+    }
+
 }
