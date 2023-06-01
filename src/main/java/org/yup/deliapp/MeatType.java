1a8286231521557047bcf81fcd8564455e793dac
Benjamin Jackson <benjacksonk@gmail.com>
2023-06-01 10:42:50 -0400

    added enum values to BreadType CheeseType MeatType FreeTopping

--- src/main/java/org/yup/deliapp/MeatType.java
+++ src/main/java/org/yup/deliapp/MeatType.java
@@ -1,10 +1,20 @@
 package org.yup.deliapp;
 
 public enum MeatType {
-    STEAK,
-    HAM,
-    SALAMI,
-    ROAST_BEEF,
-    CHICKEN,
-    BACON
+    STEAK(1),
+    HAM(2),
+    SALAMI(3),
+    ROAST_BEEF(4),
+    CHICKEN(5),
+    BACON(6);
+
+    private final int value;
+
+    MeatType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
